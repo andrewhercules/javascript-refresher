11e300616815a554c27729ebe868b09aec2a050f
Andrew Hercules <andrewhercules@gmail.com>
2015-01-20 19:41:16 -0500

    Transferred answers for questions 31-35 from master to answers branch

---
+++ src/Questions31-35.js
@@ -0,0 +1,29 @@
+// Question 31
+
+function getUpperLimitOfRange(range) {
+
+};
+
+// Question 32
+
+function squareRootOf(number) {
+
+};
+
+// Question 33
+
+function callMethodFromString(string) {
+
+};
+
+// Question 34
+
+function isA2015BankHoliday(date) {
+
+};
+
+// Question 35
+
+function convertToRomanNumerals(number) {
+
+};
